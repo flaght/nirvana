930c763a8b6bd346f1ffc583a7edcd2d0c55f511
kerry <kerry@miglab.com>
2018-01-25 14:21:18 +0800

    修改文件目录

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# 0 print  1. MLOG
+LOG_TYPE = 0
+BIZE_DB_FILE = '/home/kerry/work/pj/nirvana/file/bize.db'
+RECORD_BASE_DIR = '/home/kerry/work/pj/nirvana/'
